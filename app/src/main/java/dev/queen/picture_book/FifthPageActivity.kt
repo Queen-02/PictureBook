fcb2ae3a6f9ca036a0288f4f681e94ccc8079361
Queen-02 <queenzyblack176@gmail.com>
2022-05-25 22:45:58 +0300

    made changes to fifth file

--- app/src/main/java/dev/queen/picture_book/FifthPageActivity.kt
+++ app/src/main/java/dev/queen/picture_book/FifthPageActivity.kt
@@ -1,11 +1,28 @@
 package dev.queen.picture_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FifthPageActivity : AppCompatActivity() {
+    lateinit var previous: Button
+    lateinit var bckHome: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fifth_page)
+        previous = findViewById(R.id.btnPrvFour)
+        bckHome = findViewById(R.id.btnBack)
+
+        previous.setOnClickListener {
+            val intent = Intent(this, FourthPageActivity::class.java)
+            startActivity(intent)
+        }
+
+        bckHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
