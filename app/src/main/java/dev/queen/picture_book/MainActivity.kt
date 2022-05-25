aba96fc173e4fbfe0bd7be91084ae8c7f90a6c9f
Queen-02 <queenzyblack176@gmail.com>
2022-05-25 20:33:02 +0300

    Added a button in main activity

--- app/src/main/java/dev/queen/picture_book/MainActivity.kt
+++ app/src/main/java/dev/queen/picture_book/MainActivity.kt
@@ -1,11 +1,18 @@
 package dev.queen.picture_book
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nxt: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nxt = findViewById(R.id.btnNxt)
+
+        var intent = Intent(this, SecondPageActivity::class.java)
+        startActivity(intent)
     }
 }
